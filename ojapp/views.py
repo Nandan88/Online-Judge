f7e3f70f41500b3a0b3b7f2438731a98f0e00081
Nandan <nandanmahajan12@gmai.com>
2022-07-14 13:23:50 +0530

    code compile or interpret on docker

--- ojapp/views.py
+++ ojapp/views.py
@@ -63,16 +63,41 @@ def submitProblem(request,problem_id):
     # print(problem.input)
     plang=request.POST.get("languages")
     if plang=="Python":
-        f=open("solution1.py","w")
+        f=open("E:\Django\OJ\ojapp\solution1.py","w")
         f.write(str(code1))
         f.close()
-        subprocess.run(["python","solution1.py"],stdin=fin1,stdout=fout,shell=True)
+        # docker run -it --name py_cont python
+        subprocess.run(["docker","cp","E:\Django\OJ\ojapp\solution1.py","py_cont:/sol1.py"],stdin=fin1,stdout=fout,shell=True)
+        subprocess.run(["docker","exec","-i","py_cont","python","sol1.py"],stdin=fin1,stdout=fout,shell=True)
     elif plang=="Cpp":
         f1=open("E:\Django\OJ\ojapp\sol.cpp","w")
         f1.write(str(code1))
-        f1.close()
-        subprocess.run(["g++","E:\Django\OJ\ojapp\sol.cpp","-o","sol.exe"],shell=True)
-        subprocess.run([".\sol.exe"],stdin=fin1,stdout=fout,shell=True)
+        f1.close()    # docker rm container id     this to remove container (docker stop k baad ya sirf ye karna padega)
+        # subprocess.run(["docker","--rm","-it","--name","comp_cpp","gcc"])
+        # subprocess.run(["docker","exec","-d",ubuntu_bash touch /tmp/execWorks])
+        # subprocess.run(["docker","cp","E:\Django\OJ\sol.cpp","cpp_cont",":","/sol.cpp"])
+        # subprocess.run(["docker","cp","E:\Django\OJ\sol.cpp","cpp_cont:/sol.cpp"])
+        # subprocess.run(["docker","cp","E:\Django\OJ\sol.cpp","cpp_cont:./sol.exe"])
+        # ps=subprocess.run(["docker","attach","cpp_cont"])
+        # subprocess.run(["docker","exec","g++","/sol.cpp","-o","sol.exe" ],shell=True)
+        # subprocess.run(["docker","exec","-i","cpp_cont","./sol" ],stdin=fin1,stdout=fout,shell=True)
+        subprocess.run(["docker","cp","E:\Django\OJ\ojapp\sol.cpp","cpp_cont:/sol.cpp"])
+        subprocess.run(["docker","exec","-i","cpp_cont","g++","sol.cpp","-o","./sol" ],shell=True)
+        subprocess.run(["docker","exec","-i","cpp_cont","./sol" ],stdin=fin1,stdout=fout,shell=True)
+        # data=fin1.read()
+        # res = bytes(data, 'utf-8')
+        # ps=subprocess.run(["docker","exec","-i","cpp_cont","./sol" ],input=res,capture_output=True,shell=True)
+        # subprocess.run([""])
+        # ps=subprocess.run(["docker","exec","-i","cpp_cont","./sol.exe" ],input=data,capture_output=True,shell=True,universal_newlines=True)
+        # ps=ps.stdout
+        # print(ps)
+        # subprocess.run(["g++","sol.cpp","-o","sol.exe"],shell=True)
+        # subprocess.run([".\sol.exe"],stdin=fin1,stdout=fout,shell=True)
+        # subprocess.run(["docker","detach","cpp_cont"])
+        
+        # fout=ps
+        # subprocess.run(["g++","E:\Django\OJ\ojapp\sol.cpp","-o","sol.exe"],shell=True)
+        # subprocess.run([".\sol.exe"],stdin=fin1,stdout=fout,shell=True)
     fin1.close()
     fout.close()
     faout=open("E:\Django\OJ\ojapp\prob_actualout.txt","w")
@@ -113,6 +138,71 @@ def submitProblem(request,problem_id):
 
 # def verdict(request,solution_id):
 #     solution=get_object_or_404(Solution,pk=solution_id)
+
+
+# def submitProblem(request,problem_id):
+#     code1=request.POST.get("code")
+#     problem = get_object_or_404(Problem,pk=problem_id)
+#     infile='E:\Django\OJ\ojapp\\' + str(problem.code) +'.txt'
+
+#     # fin=open("E:\Django\OJ\ojapp\prob_in1.txt","w")
+#     fout=open("E:\Django\OJ\ojapp\prob1_out.txt","w")
+#     # fin.write(problem.input)
+#     # fin.close()
+#     fin1=open(infile,"r")  #input file me already input rakh diya
+#     # print(problem.input)
+#     plang=request.POST.get("languages")
+#     if plang=="Python":
+#         f=open("solution1.py","w")
+#         f.write(str(code1))
+#         f.close()
+#         subprocess.run(["python","solution1.py"],stdin=fin1,stdout=fout,shell=True)
+#     elif plang=="Cpp":
+#         f1=open("E:\Django\OJ\ojapp\sol.cpp","w")
+#         f1.write(str(code1))
+#         f1.close()
+#         subprocess.run(["g++","E:\Django\OJ\ojapp\sol.cpp","-o","sol.exe"],shell=True)
+#         subprocess.run([".\sol.exe"],stdin=fin1,stdout=fout,shell=True)
+#     fin1.close()
+#     fout.close()
+#     faout=open("E:\Django\OJ\ojapp\prob_actualout.txt","w")
+#     faout.write(str(problem.output))
+#     faout.close()
+#     out1="E:\Django\OJ\ojapp\prob_actualout.txt"
+#     out2="E:\Django\OJ\ojapp\prob1_out.txt"
+#     if (filecmp.cmp(out1,out2)):
+#         verdict='Accepted'
+#     else:
+#         verdict='Wrong Answer'
+#     s=Solution.objects.filter(username=request.user,problem=problem,verdict='Accepted')
+#     print(s.count())
+#     if s.count()==0:
+#         print(request.user)
+#         sc=Score.objects.get(user=request.user)
+#         print(sc)
+#         if sc==None:
+#             su=Score()
+#             su.user=request.user
+#             su.points=5
+#             su.save()
+#         else:
+#             sc.points+=5
+#             sc.save()
+#     solution=Solution()
+#     solution.username=request.user
+#     solution.problem=problem
+#     solution.verdict=verdict
+#     # solution.submitted_at=timezone.
+#     # IST = pytz.timezone('Asia/Kolkata')
+#     solution.submitted_at=datetime.now()
+#     solution.submitted_code=str(code1)
+#     solution.save()
+
+#     # return HttpResponseRedirect(reverse('dashboard'))
+#     return render(request,'verdict.html',{'solution':solution})
+
+# # def verdict(request,solution_id):
+# #     solution=get_object_or_404(Solution,pk=solution_id)
 
 def logout(request):
     # request.user
